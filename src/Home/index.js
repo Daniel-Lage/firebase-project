f37e3bcf33d4cfbff9f18584dab17b18ae4f8755
= <=>
2023-03-18 21:54:10 -0300

    added firebase storage

--- src/Home/index.js
+++ src/Home/index.js
@@ -6,9 +6,9 @@ import {
   TextInput,
   Image,
 } from "react-native";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { getAuth, updateProfile, sendEmailVerification } from "firebase/auth";
 
 import { AntDesign } from "@expo/vector-icons";
@@ -17,14 +17,11 @@ import { launchImageLibraryAsync, MediaTypeOptions } from "expo-image-picker";
 export default function Home({ navigation }) {
   const auth = getAuth();
   const storage = getStorage();
-  const images = ref(storage, "images");
 
   const [user, setUser] = useState(auth.currentUser);
-  const [name, setName] = useState("");
+  const nameInputRef = useRef();
   const [photoURL, setPhotoURL] = useState(user.photoURL);
   const [displayName, setDisplayName] = useState(user.displayName);
-
-  console.log(user);
 
   const [message, setMessage] = useState(
     user.emailVerified ? "None" : "Email não verificado"
@@ -40,20 +37,25 @@ export default function Home({ navigation }) {
       fetch(result.assets[0].uri)
         .then((response) => response.blob())
         .then((image) => {
+          const images = ref(storage, user.uid);
           uploadBytes(images, image).then((snapshot) => {
-            console.log(snapshot);
-            console.log("Uploaded a blob or file!");
+            getDownloadURL(snapshot.ref).then((url) => setPhotoURL(url));
           });
         });
     });
   }
 
   function configurarNome() {
-    updateProfile(user, { displayName: name }).then(() => {
-      setDisplayName(name);
-      setUser(auth.currentUser);
-    });
+    setDisplayName(nameInputRef.current.value);
   }
+
+  useEffect(() => {
+    updateProfile(user, { displayName: displayName, photoURL: photoURL }).then(
+      () => {
+        setUser(auth.currentUser);
+      }
+    );
+  }, [displayName, photoURL]);
 
   return (
     <>
@@ -68,7 +70,7 @@ export default function Home({ navigation }) {
             auth.signOut();
           }}
         >
-          <Text>Sair</Text>
+          <AntDesign name="back" size={24} color="black" />
         </Pressable>
         <Text style={{ fontWeight: "bold", userSelect: "none" }}>
           Firebased - Home
@@ -76,18 +78,25 @@ export default function Home({ navigation }) {
       </View>
       <View style={styles.container}>
         {photoURL ? (
-          <Image
-            style={{
-              height: 200,
-              width: 200,
-              backgroundColor: "rgb(212, 212, 212)",
-              borderRadius: 5,
-            }}
-            onError={(error) => {
-              setImage(null);
-            }}
-            source={photoURL}
-          />
+          <View style={{ justifyContent: "flex-end" }}>
+            <Image
+              style={{
+                height: 200,
+                width: 200,
+                backgroundColor: "rgb(212, 212, 212)",
+                borderRadius: 5,
+                borderBottomRightRadius: 72,
+              }}
+              onError={() => setImage(null)}
+              source={photoURL}
+            />
+            <Pressable
+              style={{ position: "absolute", alignSelf: "flex-end" }}
+              onPress={configurarImagem}
+            >
+              <AntDesign name="edit" size={24} color="black" />
+            </Pressable>
+          </View>
         ) : (
           <Pressable
             style={{
@@ -104,13 +113,24 @@ export default function Home({ navigation }) {
           </Pressable>
         )}
         {displayName ? (
-          <Text style={{ fontWeight: "bold" }}>{displayName}</Text>
+          <View style={styles.nameDisplay}>
+            <Text style={{ fontWeight: "bold", marginHorizontal: 24 }}>
+              {displayName}
+            </Text>
+            <Pressable
+              style={{ position: "absolute", alignSelf: "flex-end" }}
+              onPress={() => {
+                setDisplayName(null);
+              }}
+            >
+              <AntDesign name="edit" size={24} color="black" />
+            </Pressable>
+          </View>
         ) : (
           <TextInput
             placeholder="Nome"
             style={styles.textField}
-            value={name}
-            onChangeText={setName}
+            ref={nameInputRef}
             onKeyPress={(e) => {
               if (e.keyCode === 13) configurarNome();
             }}
@@ -171,5 +191,12 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: "rgb(46, 46, 46)",
     backgroundColor: "rgb(212, 212, 212)",
+  },
+  nameDisplay: {
+    width: 200,
+    height: 40,
+    borderRadius: 5,
+    textAlign: "center",
+    justifyContent: "center",
   },
 });
